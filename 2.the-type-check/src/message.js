24c4639ccf6e59f9394fc60e35876c73f4baf442
Nathaniel <linlilulll@gmail.com>
2020-12-08 02:47:26 +0800

    rename files

--- 2.the-type-check/src/message.js
+++ 2.the-type-check/src/message.js
@@ -1,14 +1,10 @@
-/// <reference path="./models/user.d.ts" />
 /// <reference path="./message.d.ts" />
 
 const someLib = require('some-js-lib');
 const camelCase = require('lodash.camelcase');
 
 /**
- * @param {User} from
- * @param {User} to
- * @param {Message} message
- * @returns {Promise<MessageResult>}
+ * @type {sendMessage}
  */
 async function sendMessage(from, to, message) {
   return someLib.sendMessage(from.id, to.id, camelCase(message.content));
